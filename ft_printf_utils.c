03e13cfa7b1a5854e0cd2fa7d3aaf39c17fb954d
Andrpinh <andrefppereira2001@gmail.com>
2026-01-07 21:10:41 +0000

    Update ft_printf_utils.c

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -20,13 +20,15 @@ int ft_putchar(char c)
 
 int ft_putstr(char *s)
 {
-	int	i;
+	int count = 0;
 
-	i = 0;
-	while (s[i])
-	{
-		write(1, &s[i], 1);
-		i++;
-	}
-	return (i);
+    if (!s)
+        s = "(null)";
+    while (*s)
+    {
+        write(1, s, 1);
+        s++;
+        count++;
+    }
+    return (count);
 }
