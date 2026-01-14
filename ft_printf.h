2b423b444fc0c0ab68a7aa26940af540fb5cae6f
Andrpinh <andrefppereira2001@gmail.com>
2026-01-14 23:29:05 +0000

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -19,11 +19,6 @@
 int ft_printf(const char *string, ...);
 int print_format(char c, va_list args);
 
-int ft_putchar(char c);
-int ft_putstr(char *s);
-int ft_putnbr(int n);
-int ft_putunsigned(unsigned int n);
-int ft_puthex(unsigned int n, char c);
-int ft_putptr(unsigned long long n);
+
 
 #endif
